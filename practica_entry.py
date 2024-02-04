e3e722fbbdc6f22b10ec601b0d74e65b248488bf
Fredev01 <fredev08@gmail.com>
2024-02-04 12:46:47 -0600

    Practicando los componentes label

--- practica_entry.py
+++ practica_entry.py
@@ -1,33 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
 def enviar():
-    print(entrada1.get())
-    boton1.config(text=entrada1.get())
-    # Eliminar el contenido en la caja de texto
-    # entrada1.delete(0, tk.END)
-    # Seleccionar la el texto en la caja
-    entrada1.select_range(0, tk.END)
-    # para hacer efectiva la seleccion 
-    entrada1.focus()
+    # boton1.config(text=variable_ent_1.get())
+    # # modificacion utilizamos la varible de texto y el metodo set
+    # variable_ent_1.set('cambio....')
+    # recuperar la informacion ya modificada 
+    print(variable_ent_1.get())
+    etiqueta1.config(text=variable_ent_1.get())
     
 ventana = tk.Tk()
 ventana.geometry('1500x1200')
-ventana.title('Practicas con entry')
-# ventana.rowconfigure(0, weight=2)
-# width es la cantidad de caracteres
-# la propiedad show sirve para ocultar el texto escrito por el user, util para password
-# entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER, show='*')
-# propiedad state para deshabilitar el campo 
-# entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER, state=tk.DISABLED)
-entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER)
+ventana.title('Manejo de componentes')
+ventana.rowconfigure(0, weight=1)
+etiqueta1 = tk.Label(ventana,text='Nombre:',font='arial')
+variable_ent_1 = tk.StringVar(value='Valor por default')
+entrada1 = ttk.Entry(ventana, width=30,textvariable=variable_ent_1)
 
-entrada1.grid(row=0, column=0)
+entrada1.grid(row=0, column=1, padx=20, pady=20, ipadx=20, sticky='NS')
+etiqueta1.grid(row=0, column=0, padx=20, pady=20, sticky='NS')
 # insert agrega un texto
-entrada1.insert(0,'Introduce una cadena')
-entrada1.insert(tk.END, '.')
+# entrada1.insert(0,'Introduce una cadena')
+# entrada1.insert(tk.END, '.')
 # entrada1.configure(state='readonly')
 
 
 boton1 = ttk.Button(ventana, text='enviar', command=enviar)
-boton1.grid(row=0, column=1)
+boton1.grid(row=0, column=2, padx=20, pady=15, sticky='NS')
 ventana.mainloop()
